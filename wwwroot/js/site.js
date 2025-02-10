aaa03fae586ce23f165c36067bd6af9d8d93a286
Kurumizawaa <1847123@gmail.com>
2025-02-10 23:24:50 +0700

    add announcement fetching (not show in view yet)

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,3 +3,17 @@ portrait_menu.onclick = function(){
     nav_bar = document.querySelector(".nav-bar");
     nav_bar.classList.toggle("active");
 }
+
+const notification = document.getElementById("notifications");
+
+function get_notification() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let announcement_list = JSON.parse(xhttp.response);
+            console.log(announcement_list);
+        }
+    };
+    xhttp.open("GET", "/Announcement/GetAnnouncement", true);
+    xhttp.send();
+}
