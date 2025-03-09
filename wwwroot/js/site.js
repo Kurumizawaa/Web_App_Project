b69a41beb8659376014c3f1002c26862902d3e24
Kurumizawaa <1847123@gmail.com>
2025-03-10 01:17:38 +0700

    add default profile picture.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -49,7 +49,7 @@ function insert_notification(announcement_list) {
         notif.dataset.postid = announcement.postid;
         notif.dataset.announcementid = (announcement.isread == false) ? announcement.id : null;
         notif.innerHTML = `
-            <img src="${announcement.picture ?? "../images/jaikere.PNG"}" alt="notif-img" class="notif-img">
+            <img src="${announcement.type != 3 ? announcement.picture : "../images/jaikere.PNG"}" alt="notif-img" class="notif-img" ${announcement.picture != null ? "" : "hidden"}>
             <div class="notif-text-container">
                 <h3 class="notif-text-header">${header} <span></span></h3>
                 <p class="notif-text"></p>
